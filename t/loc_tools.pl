c64af7639fe68976128ad6014c2434d521d7cfba
Karl Williamson <khw@cpan.org>
2016-11-27 10:42:09 -0700

    Revert "loc_tools.pl: Debug statements"

--- t/loc_tools.pl
+++ t/loc_tools.pl
@@ -73,7 +73,6 @@ sub _trylocale ($$$$) { # For use only by other functions in this file!
     my $list = shift;
     my $allow_incompatible = shift;
 
-    print STDERR "# trying '$locale'\n";
     return if ! $locale || grep { $locale eq $_ } @$list;
 
     $categories = [ $categories ] unless ref $categories;
@@ -85,10 +84,7 @@ sub _trylocale ($$$$) { # For use only by other functions in this file!
 
     local $SIG{__WARN__} = sub {
         $badutf8 = 1 if $_[0] =~ /Malformed UTF-8/;
-        $plays_well = 0 if $_[0] =~ /Locale .* may not work well/i;
-        my $warn = $_[0];
-        chomp $warn;
-        print STDERR "# setlocale(foo, $locale) gave this warning: $warn\n";
+        $plays_well = 0 if $_[0] =~ /Locale .* may not work well/i
     };
 
     # Incompatible locales aren't warned about unless using locales.
@@ -98,11 +94,7 @@ sub _trylocale ($$$$) { # For use only by other functions in this file!
         die "category '$category' must instead be a number"
                                             unless $category =~ / ^ -? \d+ $ /x;
 
-        print STDERR "# Calling setlocale($category_name{$category}, '$locale')\n";
-        my $ret = setlocale($category, $locale);
-        print STDERR "# setlocale($category, '$locale') failed\n" unless $ret;
-        return unless $ret;
-        #return unless setlocale($category, $locale);
+        return unless setlocale($category, $locale);
         return if ! $plays_well && ! $allow_incompatible;
     }
 
